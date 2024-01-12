624da6234ab0df4a3c8acf37d06e46a8df859192
AnibalAbrilDJ <anibalabril0324@gmail.com>
2024-01-11 23:06:28 -0500

    Estilos en los formularios

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Estudiante
 
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Estudiante 
-        fields = ['cedula','nombre','apellido','direccion','telefono','user']      
+        fields = ['cedula','nombre','apellido','direccion','telefono','user']
+        widgets = {
+            'cedula': forms.TextInput(attrs={'class': 'form-control' ,'placeholder':'Escribe tu cedula'}),
+            'nombre': forms.TextInput(attrs={'class': 'form-control' ,'placeholder':'Escribe tu nombre'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control' ,'placeholder':'Escribe tu apellido'}),
+            'direccion': forms.TextInput(attrs={'class': 'form-control' ,'placeholder':'Escribe tu direccion'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control' ,'placeholder':'Escribe tu telefono'}),
+        }       
